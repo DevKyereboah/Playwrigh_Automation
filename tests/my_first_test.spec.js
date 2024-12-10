c64bf1467f8aeadfb8564d8778ff63ea9d1d871c
DevKyereboah <117347003+DevKyereboah@users.noreply.github.com>
2024-12-10 09:26:29 +0000

    initial commit

---
+++ tests/my_first_test.spec.js
@@ -0,0 +1,13 @@
+const { test, expect } = require('@playwright/test');
+
+test('first test', async ({page}) =>{
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({page}) =>{
+    await page.goto('https://playwright.dev/');
+    await page.getByRole('link', {name: 'Get started'}).click();
+
+    await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
+})
